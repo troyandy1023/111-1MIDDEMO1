16c0cbf4056d3e1c9bbaa64301f7c2347f4d313c
altoliaw <altoliaw@hotmail.com>
2022-10-22 20:25:49 +0800

    Q2題目設計

--- 111-1MIDDEMO1/Q2.aspx.cs
+++ 111-1MIDDEMO1/Q2.aspx.cs
@@ -7,6 +7,12 @@ using System.Web.UI.WebControls;
 
 namespace _111_1MIDDEMO1 {
     public partial class Q2 : System.Web.UI.Page {
+        string[] s_City = new string[2] { "台北市", "新北市" };
+        string[,] s_Area = new string[2, 3] {
+            {"中正區", "文山區", "大安區"},
+            {"淡水區", "石碇區", "土城區"}
+        };
+
         protected void Page_Load(object sender, EventArgs e) {
 
         }
